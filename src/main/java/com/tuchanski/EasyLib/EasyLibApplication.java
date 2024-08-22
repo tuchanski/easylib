af33d5e0b40f3b02a5045ad0df22f48d62014b42
Tuchanski <gui.rocha2004@gmail.com>
2024-08-22 10:33:02 -0300

    Create project

---
+++ src/main/java/com/tuchanski/EasyLib/EasyLibApplication.java
@@ -0,0 +1,13 @@
+package com.tuchanski.EasyLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuchanski/EasyLib/EasyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuchanski.EasyLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
