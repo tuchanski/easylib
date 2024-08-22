e0febf2349d5c29b6aa94904fd702ef8de7ab583
Tuchanski <gui.rocha2004@gmail.com>
2024-08-22 10:44:17 -0300

    Add book entity

---
+++ src/main/java/com/tuchanski/EasyLib/models/Book.java
@@ -0,0 +1,41 @@
+package com.tuchanski.EasyLib.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Setter(AccessLevel.PROTECTED)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private String description;
+
+}
