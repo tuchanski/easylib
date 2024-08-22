a5994aa051560f8c7fe7e5adb747418d75d3c732
Tuchanski <gui.rocha2004@gmail.com>
2024-08-22 10:46:05 -0300

    Add repository notation on book repository

--- src/main/java/com/tuchanski/EasyLib/repositories/BookRepository.java
+++ src/main/java/com/tuchanski/EasyLib/repositories/BookRepository.java
@@ -3,9 +3,11 @@ package com.tuchanski.EasyLib.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tuchanski.EasyLib.models.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
     
 }
