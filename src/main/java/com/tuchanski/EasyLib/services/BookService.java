b4d2a3b970690665b085fc5ba18985d0602ab6e3
Tuchanski <gui.rocha2004@gmail.com>
2024-08-22 17:20:12 -0300

    Refactor update method in book service

--- src/main/java/com/tuchanski/EasyLib/services/BookService.java
+++ src/main/java/com/tuchanski/EasyLib/services/BookService.java
@@ -24,7 +24,6 @@ public class BookService {
         return ResponseEntity.status(HttpStatus.OK).body(bookRepository.findAll());
     }
 
-    // CREATE
     public ResponseEntity<Object> createBook(BookRecordDTO bookDTO) {
         Book newBook = new Book();
         
@@ -44,7 +43,6 @@ public class BookService {
         }
     }
 
-    // DELETE
     public ResponseEntity<Object> deleteBook(UUID id){
 
         Optional<Book> toBeDeleted = bookRepository.findById(id);
@@ -57,7 +55,6 @@ public class BookService {
         return ResponseEntity.status(HttpStatus.OK).body("Book with ID " + id + " has been deleted");
     }
 
-    // UPDATE
     public ResponseEntity<Object> updateBook(UUID id, BookRecordDTO newInfoDTO){
 
         Optional<Book> existingBookOpt = bookRepository.findById(id);
@@ -86,9 +83,7 @@ public class BookService {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected error: " + e.getMessage());
         }
-
     }
-
 
     private BookGenre getGenreByDisplayName(String displayName){
         for (BookGenre genre : BookGenre.values()){
@@ -98,6 +93,5 @@ public class BookService {
         }
         throw new IllegalArgumentException("Genre not found: " + displayName);
     }
-
 
 }
